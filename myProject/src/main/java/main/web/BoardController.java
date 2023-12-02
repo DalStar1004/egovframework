6ed93a1379c4495274561d230607c549571531b5
cupid <cupid@112.167.160.121>
2023-12-03 00:39:29 +0900

    ■ 실행환경 - jdk1.8.0_361 - jdk-21 - jre-1.8
    ■ 오라클버전
    - Oracle Database 21c Express Edition Release 21.0.0.0.0 - Production
    
    ■ 오라클 사용계정
    - 사용자이름 : ora_master / 비번 : 1234
    - host : localhost / port : 1521 / service-name : XE
    
    ■ DB실행툴
    - QueryBox

--- myProject/src/main/java/main/web/BoardController.java
+++ myProject/src/main/java/main/web/BoardController.java
@@ -1,14 +1,59 @@
 package main.web;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import main.service.BoardService;
+import main.service.BoardVO;
 
 @Controller
 public class BoardController {
+	
+	    @Resource(name="boardService")
+	    private BoardService boardService;
 
 		@RequestMapping("/boardWrite.do")
 		public String boardWrite() {
 			
 			return "board/boardWrite";
+		}
+		
+		
+		
+		@RequestMapping("/boardWriteSave.do")
+		@ResponseBody
+		public String insertNBoard(BoardVO vo) throws Exception{
+			// result = null; (insert 정상실행시  null)
+			String result = boardService.insertNBoard(vo);
+			String msg = "";
+			if(result == null) msg = "ok";
+			else msg = "fail";
+			
+			return msg; 
+			
+		}
+		
+		@RequestMapping("/boardList.do")
+		public String selectNBoardList(BoardVO vo, ModelMap model) throws Exception{
+			
+			// 총 데이터 개수 
+			int total = boardService.selectNBoardTotal(vo);
+			
+			// (double)12/10 -> 1.2 -> Integer(2.0) -> 2
+			int totalPage = (int) Math.ceil((double)total/10);
+			
+			List<?> list = boardService.selectNBoardList(vo);
+			System.out.println("list: "+list);
+						 
+			model.addAttribute("total", total);
+			model.addAttribute("totalPage", totalPage);
+			model.addAttribute("resultList", list);
+			return "board/boardList";
 		}
 }
--- myProject/src/main/webapp/dept/deptWrite.jsp
+++ myProject/src/main/webapp/dept/deptWrite.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 
 <!DOCTYPE html>
 <html>
@@ -8,43 +8,43 @@
 <title>Insert title here</title>
 </head>
 <style>
-table{
-	width:400px;
-	border-collapse:collapse;
-	}
-	
-th,td{
-	border:1px solid #cccccc;
-	padding:5px;
+table {
+	width: 400px;
+	border-collapse: collapse;
+}
+
+th, td {
+	border: 1px solid #cccccc;
+	padding: 5px;
 }
 </style>
 
 
 <body>
-<form name ="frm" method="post" action ="deptWriteSave.do">
-<table>
-	<tr>
-		<th>부서번호</th>
-		<td><input type="text" name="deptno"></td>
-	</tr>
-	
-	<tr>
-		<th>부서이름</th>
-		<td><input type="text" name="dname"></td>
-	</tr>
-	
-	<tr>
-		<th>부서위치</th>
-		<td><input type="text" name="loc"></td>
-	</tr>
+	<form name="frm" method="post" action="deptWriteSave.do">
+		<table>
+			<tr>
+				<th>부서번호</th>
+				<td><input type="text" name="deptno"></td>
+			</tr>
 
-	<tr>
-		<th colspan="2">
-			<button type = "submit">저장</button>
-		</th>
-	</tr>
+			<tr>
+				<th>부서이름</th>
+				<td><input type="text" name="dname"></td>
+			</tr>
 
-</table>
-</form>
+			<tr>
+				<th>부서위치</th>
+				<td><input type="text" name="loc"></td>
+			</tr>
+
+			<tr>
+				<th colspan="2">
+					<button type="submit">저장</button>
+				</th>
+			</tr>
+
+		</table>
+	</form>
 </body>
 </html>
